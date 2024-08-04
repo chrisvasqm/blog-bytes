5875e43a3e9e2b6173afff58eb0ca98b20413a9d
chrisvasqm <christianvasquezmelo@gmail.com>
2024-08-04 16:43:01 -0400

    Add bottom margin

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { AppBar, Container, Typography } from '@mui/material';
 
 function Navbar() {
   return (
-    <AppBar position='static' sx={{ padding: 2 }}>
+    <AppBar position='static' sx={{ padding: 2, marginBottom: 2 }}>
       <Container>
         <Typography fontSize={24} fontWeight={500}>Blog Bytes</Typography>
       </Container>
